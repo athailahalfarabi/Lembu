5d0f569bfbf9f563be7f9f224c7cfac24d27bf36
athailahalfarabi <afarobi0@gmail.com>
2026-06-23 19:51:40 +0700

    astro.mjs update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://lembu-five.vercel.app',
-  trailingSlash: 'ignore',
+  trailingSlash: 'never',
   integrations: [sitemap()],
 });
